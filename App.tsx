6a957502650ea670f81e03d4ddcb225c082e32b3
ksasha79 <59065517+ksasha79@users.noreply.github.com>
2025-12-17 01:10:17 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
+import Reviews from './components/Reviews';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -15,6 +16,7 @@ function App() {
         <Services />
         <Portfolio />
         <About />
+        <Reviews />
         <Contact />
       </main>
       <Footer />
