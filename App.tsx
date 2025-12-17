a2e4a1aed91b7ce7133feb25a78b4d1b2eb48854
ksasha79 <59065517+ksasha79@users.noreply.github.com>
2025-12-17 21:05:19 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,13 +4,11 @@ import Stats from './components/Stats';
 import Services from './components/Services';
 import Steps from './components/Steps';
 import Portfolio from './components/Portfolio';
-import Pricing from './components/Pricing';
 import About from './components/About';
 import Reviews from './components/Reviews';
 import FAQ from './components/FAQ';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import FloatingBot from './components/FloatingBot';
 
 function App() {
   return (
@@ -22,16 +20,12 @@ function App() {
         <Services />
         <Steps />
         <Portfolio />
-        <Pricing />
         <About />
         <Reviews />
         <FAQ />
         <Contact />
       </main>
       <Footer />
-      
-      {/* Interactive elements */}
-      <FloatingBot />
       
       {/* Sticky Mobile Call Button */}
       <div className="fixed bottom-6 right-6 z-40 md:hidden">
