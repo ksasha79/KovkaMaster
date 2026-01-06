709911d479bb87e3d686d84a77da31f771ab3d19
ksasha79 <59065517+ksasha79@users.noreply.github.com>
2026-01-06 06:31:00 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -39,3 +39,18 @@ if (container) {
     `;
   }
 }
+
+
+///import React from 'react';
+///import { createRoot } from 'react-dom/client';
+///import App from './App.tsx';
+
+///const container = document.getElementById('root');
+///if (container) {
+///  const root = createRoot(container);
+///  root.render(
+///    <React.StrictMode>
+///      <App />
+///    </React.StrictMode>
+///  );
+}
